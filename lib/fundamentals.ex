47aff25512e801eca92aa3a1f1d25054c9b52108
ryuzaki <ryuzaki>
2021-05-29 19:37:56 +0530

    persist data in a file

--- lib/fundamentals.ex
+++ lib/fundamentals.ex
@@ -1,4 +1,6 @@
 defmodule Fundamentals do
+  @db_path "users.bin"
+
   # list
   @users [
     # map
@@ -11,12 +13,20 @@ defmodule Fundamentals do
 
   # Fundamentals.add_user(%{name: "name2", email: "email2", password: "password2"})
   def add_user(user) do
-    [user | @users]
-
-    # or
-    # @users ++ user
+    new_Data = [user | get_users()]
+    File.write!(@db_path, :erlang.term_to_binary(new_Data))
+    new_Data
   end
 
-  # iex => Fundamentals.log
-  def log, do: @users
+  defp load do
+    if File.exists?(@db_path) do
+      :erlang.binary_to_term(File.read!(@db_path))
+    else
+      starting_value = []
+      File.write!(@db_path, :erlang.term_to_binary(starting_value))
+      starting_value
+    end
+  end
+
+  def get_users, do: load()
 end
