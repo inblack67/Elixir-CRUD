37342e0caeaf595353eb36cb04c052b20c4d6647
ryuzaki <ryuzaki>
2021-05-29 20:07:25 +0530

    auto incr id

--- lib/entities/user.ex
+++ lib/entities/user.ex
@@ -1,6 +1,7 @@
 defmodule Fundamentals.Entities.User do
+  # id can be null
   @enforce_keys [:name, :email, :password]
-  defstruct [:name, :email, :password]
+  defstruct [:id, :name, :email, :password]
 end
 
 # user = %User{name: "john", email: "john@gmail.com", password: "123"}
--- lib/fundamentals.ex
+++ lib/fundamentals.ex
@@ -3,14 +3,23 @@ defmodule Fundamentals do
 
   alias Fundamentals.Entities.User
 
+  def get_users, do: load()
+
   # Fundamentals.add_user(%{name: "name2", email: "email2", password: "password2"})
   # pattern matching the argument
   # user = %User{name: "name1", email: "email1", password: "password1"}
   # Fundamentals.add_user(user)
-  def add_user(%User{} = user) do
-    new_Data = [user | get_users()]
-    File.write!(@db_path, :erlang.term_to_binary(new_Data))
-    new_Data
+
+  # id should be nil in user's input
+  def add_user(%User{id: nil} = user) do
+    last_id = get_last_id()
+
+    # struct creates or updates a struct
+    user_with_id = struct(user, %{id: last_id + 1})
+
+    updated_users = [user_with_id | get_users()]
+    File.write!(@db_path, :erlang.term_to_binary(updated_users))
+    updated_users
   end
 
   defp load do
@@ -23,5 +32,15 @@ defmodule Fundamentals do
     end
   end
 
-  def get_users, do: load()
+  defp get_last_id do
+    if get_users() === [] do
+      0
+    else
+      get_users()
+      # get user with latest id (max)
+      |> Enum.max_by(fn user -> user.id end)
+      # we want just the id
+      |> Map.get(:id)
+    end
+  end
 end
